ebc1866617ddd53912b642bbdeaa2f7cc693dcea
Nitesh-Mann <maannitesh13@gmail.com>
2025-01-31 17:34:12 +0530

    responsive

--- script/index.js
+++ script/index.js
@@ -1,5 +1,5 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1.5,
+    slidesPerView: 1,
     spaceBetween: 10,
     loop:true,
     freeMode: false,
@@ -49,4 +49,39 @@ var swiper = new Swiper(".mySwiper", {
             body.style.overflow = "auto"; // Enable scrolling
         }
     });
+});
+
+  // scroll to top //
+  document.addEventListener("DOMContentLoaded", function () {
+    // Check the window's scroll position on page load and toggle the scale of the "to-top" button
+    toggleToTopButton();
+  
+    // Toggle the scale of the "to-top" button on scroll
+    window.addEventListener("scroll", toggleToTopButton);
+  
+    // Scroll to top when the "to-top" button is clicked
+    document.querySelector(".to-top").addEventListener("click", function () {
+      window.scrollTo({ top: 0, behavior: "smooth" }); // Scroll to top with smooth behavior
+    });
+  
+    // Function to toggle the scale of the "to-top" button
+    function toggleToTopButton() {
+      const toTopButton = document.querySelector(".to-top");
+      if (window.scrollY === 0) {
+        toTopButton.style.transform = "scale(0)";
+      } else {
+        toTopButton.style.transform = "scale(1)";
+      }
+    }
+});
+
+
+document.getElementById("menu-toggle").addEventListener("change", function () {
+  document.querySelector(".header2").classList.toggle("show-header");
+
+  if (document.querySelector(".header2").classList.contains("show-header")) {
+    document.body.style.overflow = "hidden"; // Disable scrolling
+  } else {
+    document.body.style.overflow = ""; // Enable scrolling
+  }
 });
